1066230563ea1babebd98f4204e386b1282c6bf2
WojciechMat <wmatsad@gmail.com>
2024-08-19 21:48:44 +0200

    download_model script

--- midi_datasets/MidiTokenizedDataset/MidiTokenizedDataset.py
+++ midi_datasets/MidiTokenizedDataset/MidiTokenizedDataset.py
@@ -5,10 +5,10 @@ import numpy as np
 import fortepyan as ff
 from midi_tokenizers.no_loss_tokenizer import ExponentialTimeTokenizer
 from datasets import Split, Dataset, DatasetInfo, GeneratorBasedBuilder
-from midi_datasets.MidiTokenizedDataset.MidiTokenizedDatasetConfig import BUILDER_CONFIGS, MidiTokenizedDatasetConfig
 
 from artifacts import special_tokens
 from data.augmentation import augment_dataset
+from midi_datasets.MidiTokenizedDataset.MidiTokenizedDatasetConfig import BUILDER_CONFIGS, MidiTokenizedDatasetConfig
 
 # NOTE: If you make some changes here, you might want to delete your huggingface cache
 # at ~/.cache/huggingface/ to rebuild the datasets
