5b077b7f25d7c8a780d61a687e86260c75ce6a57
Tomek Roszczynialski <roszcz@users.noreply.github.com>
2024-10-15 11:31:50 +0200

    Update save_augmented_dataset.py

--- scripts/save_augmented_dataset.py
+++ scripts/save_augmented_dataset.py
@@ -8,6 +8,9 @@ HF_WRITE_TOKEN = os.environ["HF_WRITE_TOKEN"]
 
 
 def prepare_piano_dataset(extra_datasets: list[str]):
+    # *base_dataset* is NOT used at all within this script
+    # - we probably should think about a dataset management design
+    # that wouldn't require us to provide it here anyway
     dataset_config = {
         "base_dataset_name": "roszcz/maestro-sustain-v2",
         "extra_datasets": extra_datasets,
@@ -29,7 +32,10 @@ def prepare_piano_dataset(extra_datasets: list[str]):
 
 
 if __name__ == "__main__":
-    dataset = prepare_piano_dataset([])
+    # This implementation of the dataset builder uses the *base_dataset* only to create
+    # validation splits, so in order to get the augmented train split, we have to pass i
+    # as an "extra_dataset"
+    dataset = prepare_piano_dataset(["roszcz/maestro-sustain-v2"])
     dataset = dataset["train"]
     dataset.push_to_hub("wmatejuk/maestro-augmented", token=HF_WRITE_TOKEN, private=True)
     dataset = prepare_piano_dataset(["roszcz/giant-midi-sustain-v2"])
